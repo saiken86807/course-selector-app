710c28f537cee062256f15a4f6e6de93df02411c
Stefanie Aiken <stefanie.aiken@gmail.com>
2020-07-12 23:34:34 -0400

    added courses to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import CourseList from './CatalogComponents/CourseList';
+import {CourseProvider} from './CatalogComponents/CourseCatalog';
 
 function App() {
   return (
     <div className="App">
      <h1>React App</h1>
      <h2>Course Selector App</h2>
+     <CourseProvider>
+       <CourseList />
+     </CourseProvider>
     </div>
   );
 }
