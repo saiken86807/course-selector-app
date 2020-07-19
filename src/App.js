901a19d70134683d181b2fcae314b0d52e18ba6e
Stefanie Aiken <stefanie.aiken@gmail.com>
2020-07-19 17:14:39 -0400

    testing with new pages created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
 import CourseList from './CatalogComponents/CourseList';
-import {CourseProvider} from './CatalogComponents/CourseCatalog';
+import { CourseProvider } from './CatalogComponents/CourseCatalog';
+import Dropdown from './CatalogComponents/DropdownMenu';
 
 function App() {
-  return (
-    <div className="App">
-     <h1>React App</h1>
-     <h2>Course Selector App</h2>
-     <CourseProvider>
-       <CourseList />
-     </CourseProvider>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>React App</h1>
+			<h2>Course Selector App</h2>
+			<CourseProvider>
+				<Dropdown />
+
+				<CourseList />
+			</CourseProvider>
+		</div>
+	);
 }
 
 export default App;
