088227d647008d5974ab8af6b94f449da1bbc9cf
Stefanie Aiken <stefanie.aiken@gmail.com>
2020-07-19 17:14:11 -0400

    added button and style with material-ui

--- src/CatalogComponents/Course.js
+++ src/CatalogComponents/Course.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import CourseList from './CourseList';
+import SelectedButton from './buttons';
 
-const Course = ({name, description, times, location}) => {
-    return (
-        <div className="courses">
-            <h2>{name}</h2>
-            <p>{description}</p>
-            <h3>{times}</h3>
-            <h3>{location}</h3>
-        </div>
-    );
+const Course = ({ name, description, times, location }) => {
+	return (
+		<div className="courses">
+			<h2>{name}</h2>
+			<p>{description}</p>
+			<h3>{times}</h3>
+			<h3>{location}</h3>
+			<SelectedButton />
+		</div>
+	);
 };
 
 export default Course;
