3e23bccdb10e7a70825c004b6e7d167abb628785
Stefanie Aiken <stefanie.aiken@gmail.com>
2020-07-19 20:21:52 -0400

    created simple appbar/navbar to start

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import CourseList from './CatalogComponents/CourseList';
 import { CourseProvider } from './CatalogComponents/CourseCatalog';
 import Dropdown from './CatalogComponents/DropdownMenu';
+import ClassSelectionForm from './Form Components/ClassSelectionForm';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>React App</h1>
-			<h2>Course Selector App</h2>
+			<AppBar title="Enter your selected courses" position="static">
+				<Toolbar>
+					<Typography variant="h6">Course Selection</Typography>
+				</Toolbar>
+			</AppBar>
 			<CourseProvider>
-				<Dropdown />
-
-				<CourseList />
+				{/* <Dropdown /> */}
+				<ClassSelectionForm />
+				{/* <CourseList /> */}
 			</CourseProvider>
 		</div>
 	);
