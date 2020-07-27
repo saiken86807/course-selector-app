b80cdaffb9d2aafb9c1ffa355694e21a1f284a04
Stefanie Aiken <stefanie.aiken@gmail.com>
2020-07-26 23:10:00 -0400

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import './App.css';
-import CourseList from './CatalogComponents/CourseList';
-import { CourseProvider } from './CatalogComponents/CourseCatalog';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import AppBar from '@material-ui/core/AppBar';
-import CourseSelectionForm from './Form Components/CourseSelectionForm';
 import Layout from './PageLayout/Layout';
 
 function App() {
