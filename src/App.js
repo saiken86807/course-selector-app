ac9bbb8c2fc98176ad06d33c764cfbb22b6038e1
Stefanie Aiken <stefanie.aiken@gmail.com>
2020-07-26 23:09:43 -0400

    Removed AppBar to Layout File & cleaned up file

--- src/App.js
+++ src/App.js
@@ -6,20 +6,12 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import CourseSelectionForm from './Form Components/CourseSelectionForm';
+import Layout from './PageLayout/Layout';
 
 function App() {
 	return (
 		<div className="App">
-			<AppBar title="Enter your selected courses" position="static">
-				<Toolbar>
-					<Typography variant="h6">Course Selection</Typography>
-				</Toolbar>
-			</AppBar>
-
-			<CourseProvider>
-				<CourseSelectionForm className="CourseSelection" />
-				<CourseList className="CourseList" />
-			</CourseProvider>
+			<Layout />
 		</div>
 	);
 }
