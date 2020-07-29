296c89ef7585f94ec67adc3b25363fd284296deb
Stefanie Aiken <stefanie.aiken@gmail.com>
2020-07-28 22:33:48 -0400

    removed imports not in use

--- src/PageLayout/Layout.js
+++ src/PageLayout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -10,7 +10,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import CourseSelectForm from './CourseSelectForm';
 import CourseTabIndex from './CourseTabIndex';
 import CreditEvaluation from './CreditEvaluation';
-import { blue } from '@material-ui/core/colors';
 
 function TabPanel(props) {
 	const { children, value, index, ...other } = props;
@@ -48,8 +47,7 @@ function a11yProps(index) {
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
-		backgroundColor: theme.palette.background.paper,
-		backgroundColor: blue
+		backgroundColor: theme.palette.background.paper
 	}
 }));
 
