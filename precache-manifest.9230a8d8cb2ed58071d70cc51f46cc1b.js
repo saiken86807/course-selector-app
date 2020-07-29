de945a5ed34ca3989da015438d1cdc8d3ee3a28b
Stefanie Aiken <stefanie.aiken@gmail.com>
2020-07-29 00:50:11 -0400

    Updates

---
+++ precache-manifest.9230a8d8cb2ed58071d70cc51f46cc1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856ee0f03790513238e802447a2c154e",
+    "url": "/course-selector-app/index.html"
+  },
+  {
+    "revision": "22b6d4cd9a645dc96aa0",
+    "url": "/course-selector-app/static/css/main.938cfc98.chunk.css"
+  },
+  {
+    "revision": "6ae5584cf1da2cf4ea44",
+    "url": "/course-selector-app/static/js/2.2eec2c12.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/course-selector-app/static/js/2.2eec2c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b6d4cd9a645dc96aa0",
+    "url": "/course-selector-app/static/js/main.3d156fc0.chunk.js"
+  },
+  {
+    "revision": "c9c61a24b65ac0293a59",
+    "url": "/course-selector-app/static/js/runtime-main.fa794f11.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/course-selector-app/precache-manifest.c04132e3423051c2da8a1af8cf62e768.js"
+  "/course-selector-app/precache-manifest.9230a8d8cb2ed58071d70cc51f46cc1b.js"
 );
 
 self.addEventListener('message', (event) => {
