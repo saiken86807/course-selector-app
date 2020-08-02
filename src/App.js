a03c7f33def76400aa5cb0646e772f5bf6caabf7
Stefanie Aiken <stefanie.aiken@gmail.com>
2020-08-02 15:39:57 -0400

    removed imports no longer in use

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Layout from './PageLayout/Layout';
-import SelectionForm from './FormComponents/SelectionForm';
 
 function App() {
 	return (
 		<div className="App">
-			{/* <Layout className="Layout" /> */}
-			<SelectionForm />
+			<Layout className="Layout" />
 		</div>
 	);
 }
