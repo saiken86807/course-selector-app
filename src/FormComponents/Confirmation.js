56d2b4a368dea8cd8e4677ed47cc33dabc4bea02
Stefanie Aiken <stefanie.aiken@gmail.com>
2020-08-02 23:21:11 -0400

    changed buttons and titles

--- src/FormComponents/Confirmation.js
+++ src/FormComponents/Confirmation.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Form } from 'reactstrap';
-import { List, ListItem, ListItemText } from '@material-ui/core/';
+import { List, ListItem, ListItemText, Button } from '@material-ui/core/';
 
 class Confirmation extends Component {
 	saveAndContinue = (e) => {
@@ -19,29 +19,35 @@ class Confirmation extends Component {
 		return (
 			<div>
 				<Form>
+					<h1 className="ui centered">Confirm your Selected Courses</h1>
+					<p>Click Confirm if the following details have been correctly entered</p>
 					<List>
-						<ListItem>
+						<ListItem name="course1">
 							<ListItemText name="course1" primary="Course 1:" secondary={course1} />
 						</ListItem>
-						<ListItem>
+						<ListItem name="course2">
 							<ListItemText name="course2" primary="Course 2:" secondary={course2} />
 						</ListItem>
-						<ListItem>
+						<ListItem name="course3">
 							<ListItemText name="course3" primary="Course 3: " secondary={course3} />
 						</ListItem>
-						<ListItem>
+						<ListItem name="course4">
 							<ListItemText name="course4" primary="Course 4: " secondary={course4} />
 						</ListItem>
-						<ListItem>
+						<ListItem name="altcourse1">
 							<ListItemText name="altcourse1" primary="Alternate Course 1: " secondary={altcourse1} />
 						</ListItem>
-						<ListItem>
+						<ListItem name="altcourse2">
 							<ListItemText name="altcourse2" primary="Alternate Course 2: " secondary={altcourse2} />
 						</ListItem>
 					</List>
 				</Form>
-				<button onClick={this.back}>Back</button>
-				<button onClick={this.saveAndContinue}>Confirm</button>
+				<Button color="secondary" variant="contained" onClick={this.back}>
+					Back
+				</Button>
+				<Button color="primary" variant="contained" onClick={this.saveAndContinue}>
+					Confirm
+				</Button>
 			</div>
 		);
 	}
